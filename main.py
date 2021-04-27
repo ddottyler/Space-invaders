2f6b609049219ddf50169d6699073d16bc145d35
Dan T <dp.tyler@hotmail.co.uk>
2021-04-27 12:00:05 +0100

    Laser class created. Collide method created to tell us whether two objects are overlapping

--- main.py
+++ main.py
@@ -39,6 +39,26 @@ BG = pygame.transform.scale(pygame.image.load(
 # Abstract class. Won't be used, but we will inherit from it (enemy SHIP, player SHIP etc)
 
 
+class Laser:
+    def __init__(self, x, y, img):
+        self.x = x
+        self.y = y
+        self.img = img
+        self.mask = pygame.mask.from_surface(self.img)
+
+    def draw(self, window):
+        window.blit(self.img, (self.x, self.y))
+
+    def move(self, vel):
+        self.y += vel
+
+    def off_screen(self, height):
+        return self.y <= height and self.y >= 0
+
+    def collision(self, obj):
+        return collide(obj, self)
+
+
 class Ship:
     def __init__(self, x, y, health=100):
         self.x = x
@@ -84,6 +104,12 @@ class Enemy(Ship):
 
     def move(self, vel):
         self.y += vel
+
+
+def collide(obj1, obj2):
+    offset_x = obj2.x - obj1.x
+    offset_y = obj2.y - obj1.y
+    return obj1.mask.overlap(obj2.mask, (offset_x, offset_y)) != None
 
 
 def main():
