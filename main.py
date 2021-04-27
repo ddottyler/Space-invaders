a2ac0fbe5da060da46a919ec460a26b45dd953a7
Dan T <dp.tyler@hotmail.co.uk>
2021-04-27 10:23:16 +0100

    Background image updated to add scale to the screen. redraw_window function creataed to update display

--- main.py
+++ main.py
@@ -32,8 +32,8 @@ YELLOW_LASER = pygame.image.load(
 
 # Background
 
-BG = pygame.image.load(
-    os.path.join("assets", "background-black.png"))
+BG = pygame.transform.scale(pygame.image.load(
+    os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
 
 
 def main():
@@ -41,8 +41,14 @@ def main():
     FPS = 60
     clock = pygame.time.Clock()
 
+    def redraw_window():
+        WIN.blit(BG, (0, 0))
+
+        pygame.display.update()
+
     while run:
         clock.tick(FPS)
+        redraw_window()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
